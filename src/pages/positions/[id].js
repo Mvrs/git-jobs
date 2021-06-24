8e313199af6bdbf7463289918424778121a0e03d
Marlon Johnson <johnsonmarlon18@gmail.com>
2021-06-23 23:27:36 -0700

    fix: title undefined

--- src/pages/positions/[id].js
+++ src/pages/positions/[id].js
@@ -1,27 +1,24 @@
-import { useEffect, useState } from 'react'
-import Link from 'next/link'
-import Layout from '../../components/layout/layout'
-import styles from './positions.module.css'
+import { useEffect, useState } from "react";
+import Link from "next/link";
+import Layout from "../../components/layout/layout";
+import styles from "./positions.module.css";
 // import card_style from '../../components/jobs/jobs.module.css'
-import PublicRoundedIcon from '@material-ui/icons/PublicRounded'
-import ReactMarkdown from 'react-markdown'
-import gfm from 'remark-gfm'
-import TimeAgo from 'javascript-time-ago'
-import en from 'javascript-time-ago/locale/en'
+import PublicRoundedIcon from "@material-ui/icons/PublicRounded";
+import ReactMarkdown from "react-markdown";
+import gfm from "remark-gfm";
+import TimeAgo from "javascript-time-ago";
+import en from "javascript-time-ago/locale/en";
 
-import AccessTimeIcon from '@material-ui/icons/AccessTime'
-import { makeStyles } from '@material-ui/core'
+import AccessTimeIcon from "@material-ui/icons/AccessTime";
+import { makeStyles } from "@material-ui/core";
 
 async function getJob(id) {
   const result = await fetch(
     `https://jobs.github.com/positions/${id}.json?markdown=true`,
-  )
-  const job = await result.json()
+  );
+  const job = await result.json();
 
-  console.log(job)
-
-  console.log(id)
-  return job
+  return job;
 }
 
 const useStyles = makeStyles(theme => ({
@@ -30,28 +27,28 @@ const useStyles = makeStyles(theme => ({
     // display: 'inline-flex',
     // display: 'inline-flex',
     // position: 'relative',
-    marginTop: '26px',
+    marginTop: "26px",
   },
   date_posted: {
-    marginTop: '16px',
+    marginTop: "16px",
   },
   location: {
-    marginTop: '5px',
+    marginTop: "5px",
   },
-}))
+}));
 
-TimeAgo.addLocale(en)
+TimeAgo.addLocale(en);
 
 function Job({ job }) {
-  const classes = useStyles()
-  const timeAgo = new TimeAgo('en-US')
+  const classes = useStyles();
+  const timeAgo = new TimeAgo("en-US");
 
   return (
-    <Layout title={job.title}>
+    <Layout title={job?.title}>
       <div className={styles.container}>
         <div className={styles.container_left}>
           <Link href="/">
-            <a style={{ color: '#1e86ff', fontSize: '14px' }}>
+            <a style={{ color: "#1e86ff", fontSize: "14px" }}>
               &larr;&nbsp;&nbsp;&nbsp;Back to search
             </a>
           </Link>
@@ -113,32 +110,32 @@ function Job({ job }) {
         </div>
       </div>
     </Layout>
-  )
+  );
 }
 
-export default Job
+export default Job;
 
 export async function getStaticPaths() {
-  const result = await fetch('https://jobs.github.com/positions.json')
-  const jobs = await result.json()
+  const result = await fetch("https://jobs.github.com/positions.json");
+  const jobs = await result.json();
 
   const paths = jobs.map(job => ({
     params: { id: job.id },
-  }))
+  }));
 
   return {
     paths,
     fallback: true,
-  }
+  };
 }
 
 export async function getStaticProps({ params }) {
-  const job = await getJob(params.id)
+  const job = await getJob(params.id);
 
   return {
     props: {
       job,
     },
     // revalidate: 1,
-  }
+  };
 }
