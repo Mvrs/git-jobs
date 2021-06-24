c7e4d4bbd00e009052d09b33d026fe518e800338
Marlon Johnson <johnsonmarlon18@gmail.com>
2021-06-23 23:17:28 -0700

    add: clarify that api is now deprecated

--- src/components/layout/layout.js
+++ src/components/layout/layout.js
@@ -1,10 +1,9 @@
-import Head from 'next/head'
-import styles from './layout.module.css'
-import PropTypes from 'prop-types'
-import Link from 'next/link'
-import { useEffect, useState } from 'react'
+import Head from "next/head";
+import styles from "./layout.module.css";
+import PropTypes from "prop-types";
+import Link from "next/link";
 
-function Layout({ children, title = 'Github Jobs' }) {
+function Layout({ children, title = "Github Jobs" }) {
   return (
     <div className={styles.container}>
       <Head>
@@ -12,6 +11,29 @@ function Layout({ children, title = 'Github Jobs' }) {
         <link rel="stylesheet" href="/favicon.ico" />
       </Head>
 
+      <div>
+        <span
+          style={{
+            marginBottom: 10,
+            backgroundColor: "#9055A2",
+            display: "flex",
+            flex: 1,
+            color: "white",
+            borderRadius: 4,
+            justifyContent: "center",
+          }}
+        >
+          GitJobs API has shut down. So heres a link to my &nbsp;
+          <a
+            className={styles.repo}
+            style={{ textDecoration: "underline" }}
+            href="https://github.com/Mvrs/git-jobs"
+            target="_blank"
+          >
+            code
+          </a>
+        </span>
+      </div>
       <header className={styles.header}>
         <Link href="/">
           <a>
@@ -26,12 +48,12 @@ function Layout({ children, title = 'Github Jobs' }) {
         Marlon Johnson @ devchallenghes.io
       </footer>
     </div>
-  )
+  );
 }
 
 Layout.propTypes = {
   children: PropTypes.node,
   title: PropTypes.string,
-}
+};
 
-export default Layout
+export default Layout;
